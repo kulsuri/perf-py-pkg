c95510211403d11b85cde54e0d37d4abe068847f
Kulraj Suri <kulraj.suri@gmail.com>
2023-10-02 18:20:04 +0100

    fix: test ci  worflow

--- noxfile.py
+++ noxfile.py
@@ -15,15 +15,13 @@ try:
 except ImportError:
     message = f"""\
     Nox failed to import the 'nox-poetry' package.
-
     Please install it using the following command:
-
     {sys.executable} -m pip install nox-poetry"""
     raise SystemExit(dedent(message)) from None
 
 
 package = "perf_py_pkg"
-python_versions = ["3.11", "3.10", "3.9"]
+python_versions = ["3.10", "3.9", "3.8", "3.7"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
